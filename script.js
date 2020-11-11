2aee61248d59aabafe3811a2182fea765fc8c87a
Peter Boyle <boyle.peter614@gmail.com>
2020-11-11 18:55:56 -0500

    working problems with js, currently located in index not js file

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
-var input = document.querySelector("#input")
-var saveBtn = $(".saveBtn")
 
-$(".saveBtn").on("click", function() {
+    var input = $("#input");
+    var saveBtn = $(".saveBtn");
+    var now = moment().format('H');
+    var hour = $(".hour")
+    var nine = $("#nine")
+    
+    
+    //Test moment time
+    console.log(now)
+    
+    //Establish save button click event
+    $(".saveBtn").on("click", function() {
     event.preventDefault()
-    localStorage.setItem("task", JSON.stringify(input));
-  });
+    localStorage.setItem("task", (JSON.stringify(nine)))
+    });
+    //? not saving string input?
+    
+    //Set background/relative time for timeblocks
+    function setTime(){
+      var currentTime = parseInt(now);
+     
+      var five = $("#five").attr("data-value");                                
+
+      console.log(five)
+      if (currentTime > five){
+        $(".time-block").addClass(past)
+      }
+      else if (currentTime === five){
+        $(".time-block").addClass(present)
+      }
+      else {$(".time-block").addClass(future)}
+    
+    }
+    setTime()
+    
+    
+
